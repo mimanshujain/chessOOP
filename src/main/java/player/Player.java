da7e05c2da97fd0c0baf0fecd7a7554b70847585
Mimanshu Shisodia <mimanshujain@gmail.com>
2019-02-10 23:40:25 -0500

    Update Player.java

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,10 +1,18 @@
 package player;
 
+import history.Path;
+
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class Player {
     private String name;
     private final String id;
+    private int numberOfWins;
+    //Key = Game ID
+    //Values = Gamehistory of that game
+    private Map<String, List<Path>> gameHistory;
 
     public Player(String name) {
         this.id = UUID.randomUUID().toString();
